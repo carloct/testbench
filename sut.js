914d3bb9f3d98827fae31c10f83e7492816123e9
Carlo <c.catemario@gmail.com>
2018-08-21 07:57:34 +0100

    formatting

--- sut.js
+++ sut.js
@@ -1,12 +1,3 @@
-//
-// This is only a SKELETON file for the 'Hello World' exercise. It's been provided as a
-// convenience to get you started writing code faster.
-//
+class Sut {}
 
-class HelloWorld {
-  hello() {
-    return 'Hello, World!';
-  }
-}
-
-export default HelloWorld;
+export default Sut;
