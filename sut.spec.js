05c85387d179019263cd0651230038c35c8e19e9
Carlo <c.catemario@gmail.com>
2018-08-21 07:59:48 +0100

    added test blueprint

--- sut.spec.js
+++ sut.spec.js
@@ -1,5 +1,9 @@
 import Sut from './sut';
 
 describe('Test something', () => {
+  const sut = new Sut();
 
+  test('says hello', () => {
+    expect(sut.hello()).toEqual('Hello, World!');
+  });
 });
